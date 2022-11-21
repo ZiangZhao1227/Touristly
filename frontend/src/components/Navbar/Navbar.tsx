6ae9484e88f3e892744bb92d54b4186b4cd66028
ZIANG ZHAO <zhaoziang1227@gmail.com>
2022-11-21 14:03:51 +0200

    feat: fix responsiveness for navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,45 +1,155 @@
-import "./Navbar.css";
-import { Link } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import MenuItem from "@mui/material/MenuItem";
+import { Link as RouterLink } from "react-router-dom";
 
-const NavBar = () => {
-  const ScrollToTop = () => {
-    window.scrollTo(0, 0);
+const pages = ["home", "places", "events", "activities", "tickets"];
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
   };
 
   return (
-    <div className="NavBar">
-      <header>
-        <div className="nav-container">
-          <nav className="nav-checkbox">
-            <h1 onClick={ScrollToTop} className="name-nav">
-              Touristly
-            </h1>
-            <input id="tab-nav" type="checkbox" className="tab-nav" />
-            <label htmlFor="tab-nav" className="tab-nav-label">
-              Menu
-            </label>
-            <ul className="tab-content">
-              <li>
-                <Link to="/home">Home</Link>
-              </li>
-              <li>
-                <Link to="/places">Places</Link>
-              </li>
-              <li>
-                <Link to="/events">Events</Link>
-              </li>
-              <li>
-                <Link to="/activities">Activities</Link>
-              </li>
-              <li>
-                <Link to="/tickets">Buy tickets</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    </div>
-  );
-};
+    <AppBar position="sticky">
+      <Container
+        maxWidth="xl"
+        sx={{
+          "&.MuiContainer-root": {
+            maxWidth: "none",
+          },
+        }}
+      >
+        <Toolbar disableGutters sx={{ flex: 1 }}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Tourstly
+          </Typography>
 
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {/* {pages.map((page) => (
+                    <Link component={RouterLink} to={`/${page}`} >
+                      <MenuItem key={page} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">{page} </Typography>
+                      </MenuItem>
+                    </Link>
+              ))} */}
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Link
+                    component={RouterLink}
+                    to={`/${page}`}
+                    key={page}
+                    underline="none"
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, display: "block", color: "black" }}
+                  >
+                    {page}
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Touristly
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {/* {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                <Link component={RouterLink} to={`/${page}`}>
+                  {page}
+                </Link>
+              </Button>
+            ))} */}
+            {pages.map((page) => (
+              <Link
+                component={RouterLink}
+                to={`/${page}`}
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block", mx: 2 }}
+              >
+                {page}
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
 export default NavBar;
